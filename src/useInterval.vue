924257e18f094a5becb33581f7bebf120ddb86b8
xlearns <62782792+xlearns@users.noreply.github.com>
2021-11-12 17:31:31 +0800

    Add files via upload

---
+++ src/useInterval.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<p>Interval fired: {{ counter }}</p>
+	</div>
+</template>
+<script setup lang="ts">
+import { useInterval } from "@vueuse/core";
+const { counter, pause, resume } = useInterval(200, { controls: true });
+
+let i = 0;
+window.addEventListener("click", () => {
+	if (++i % 2) {
+		pause();
+	} else {
+		resume();
+	}
+});
+</script>
